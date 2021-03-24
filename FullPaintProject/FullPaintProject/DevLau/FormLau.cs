b4c17dfa5709e3b61b4f55131f4a2e9bc6b23e09
Butura-Laurentiu-30126 <laurentiubm@mail.student.utcluj.ro>
2021-03-24 16:18:21 +0200

    form space

---
+++ FullPaintProject/FullPaintProject/DevLau/FormLau.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FullPaintProject.DevLau
+{
+    public partial class FormLau : Form
+    {
+        public FormLau()
+        {
+            InitializeComponent();
+        }
+    }
+}
