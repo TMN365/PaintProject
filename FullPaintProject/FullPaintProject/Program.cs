32f664603d7b33fad4b109c9bd08774b50e2b7a2
olariu-gabriel-30126 <gabi.olariu2@gmail.com>
2021-03-24 14:55:27 +0200

    ceva

--- FullPaintProject/FullPaintProject/Program.cs
+++ FullPaintProject/FullPaintProject/Program.cs
@@ -14,6 +14,7 @@ namespace FullPaintProject
     /// //Test Nicu 2
     /// Nicu o modificat Aici
     /// Lau o modificat aci
+    /// miau aasda
     static class Program
     {
         /// <summary>
