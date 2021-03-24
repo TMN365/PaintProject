37e14c4447b9f8717e124d5b46b8dc0efaebf3b2
olariu-gabriel-30126 <gabi.olariu2@gmail.com>
2021-03-24 16:04:43 +0200

    folder adaugat

---
+++ FullPaintProject/FullPaintProject/DevAndrei/TestEcran.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+public class TestEcran
+{
+    String sir;
+
+
+    public TestEcran()
+    {
+        this.sir = "Am fost la munte si mi-a placut.";
+
+    }
+
+    public ShowString()
+    {
+        Console.WriteLine(sir);
+    }
+
+
+}
